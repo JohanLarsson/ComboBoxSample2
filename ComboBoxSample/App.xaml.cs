8ba3b820abcee1a5952130b790d9710851f3f6f8
Johan Larsson <johan_gavle1@hotmail.com>
2019-11-21 22:09:28 +0100

    Initial.

---
+++ ComboBoxSample/App.xaml.cs
@@ -0,0 +1,8 @@
+﻿namespace ComboBoxSample
+{
+    using System.Windows;
+
+    public partial class App : Application
+    {
+    }
+}
---
+++ ComboBoxSample/ViewModel.cs
@@ -0,0 +1,39 @@
+﻿namespace ComboBoxSample
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Linq;
+    using System.Runtime.CompilerServices;
+
+    public class ViewModel : INotifyPropertyChanged
+    {
+        private StringComparison selected;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public IReadOnlyList<StringComparison> StringComparisons { get; } = Enum.GetValues(typeof(StringComparison))
+                                                                                .Cast<StringComparison>()
+                                                                                .ToArray();
+
+        public StringComparison Selected
+        {
+            get => this.selected;
+            set
+            {
+                if (value == this.selected)
+                {
+                    return;
+                }
+
+                this.selected = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
